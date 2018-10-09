31e6a5b7502e458e6427ecb246f6abc452387bfd
SunsHine <3026450053@qq.com>
2018-10-09 15:26:03 +0800

    黑马买买买

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>您好鸭</p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  
+}
+</script>
+
+<style>
+p{
+  font-size: 88px;
+}
+</style>
